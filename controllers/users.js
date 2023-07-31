b1498dfc49c3d797b3de6770f4bbafd5615fef67
Sergey Smetankin <smetankin.sd@phystech.edu>
2023-08-01 00:55:00 +0300

    3

--- controllers/users.js
+++ controllers/users.js
@@ -9,31 +9,31 @@ function createUser(req, res) {
   console.log(req);
   User.create({ name: name, about: about, avatar: avatar })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => res.status(err.name).send({ message: err.message }));
 }
 
 function getUsers(req, res) {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => res.status(err.name).send({ message: err.message }));
 }
 
 function getUserByID(req, res) {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => res.status(err.name).send({ message: err.message }));
 }
 
 function updateUser(req, res) {
   User.findByIdAndUpdate(req.params._id, req.body, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => res.status(err.name).send({ message: err.message }));
 }
 
 function updateAvatar(req, res) {
   User.findByIdAndUpdate(req.params._id, req.body, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => res.status(err.name).send({ message: err.message }));
 }
 
 module.exports = {
