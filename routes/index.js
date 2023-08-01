b589741539acec934295efa01526ddc5df6e218f
Sergey Smetankin <smetankin.sd@phystech.edu>
2023-08-01 03:14:38 +0300

    11

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ const cardsRoute = require("./cards");
 router.use("/", usersRoute);
 router.use("/", cardsRoute);
 
+router.use("/*", (req, res) => {
+  res.status(404).send({ message: "Страница такой нету." });
+});
+
 module.exports = router;
