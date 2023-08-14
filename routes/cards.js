7804124976221ec93ddb69717ff8338d979f3286
Sergey Smetankin <smetankin.sd@phystech.edu>
2023-08-14 18:37:12 +0300

    1412

--- routes/cards.js
+++ routes/cards.js
@@ -10,7 +10,20 @@ const {
   delLikeCard,
 } = require("../controllers/cards");
 
-router.post("/cards", createCard);
+router.post(
+  "/cards",
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string().required().min(2).max(30),
+        link: Joi.string().pattern(
+          /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/
+        ),
+      })
+      .unknown(true),
+  }),
+  createCard
+);
 router.get("/cards", getCards);
 router.delete("/cards/:cardId", deleteCardByID);
 
